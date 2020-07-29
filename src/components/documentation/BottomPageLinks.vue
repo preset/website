a6c359ad3847191f362fae2bc7cee915f6c8aea5
Enzo Innocenzi <enzo@innocenzi.dev>
2020-07-29 23:34:35 +0200

    refactor: better bottom page links responsiveness

--- src/components/documentation/BottomPageLinks.vue
+++ src/components/documentation/BottomPageLinks.vue
@@ -1,39 +1,44 @@
 <template>
 	<div>
-		<div class="flex flex-col items-center justify-between sm:flex-row">
-			<g-link
-				v-if="prev"
-				:to="prev.path"
-				:class="[
-					'px-4 py-2',
-					'flex items-center',
-					'rounded-lg',
-					'font-bold',
-					'text-on-button bg-button',
-					'hover:text-on-button-hover',
-					'transition-colors duration-200',
-				]"
-			>
-				<icon icon="leftArrow" class="mr-2 text-2xl" />
-				{{ prev.title }}
-			</g-link>
+		<div class="flex flex-col justify-between sm:flex-row">
+			<div>
+				<g-link
+					v-if="prev"
+					:to="prev.path"
+					:class="[
+						'w-full px-4 py-2 mb-4 sm:mb-0',
+						'flex items-center justify-center sm:justify-start',
+						'rounded-lg',
+						'font-bold',
+						'text-on-button bg-button',
+						'hover:text-on-button-hover',
+						'transition-colors duration-200',
+					]"
+				>
+					<icon icon="leftArrow" class="mr-2 text-2xl" />
+					{{ prev.title }}
+				</g-link>
+			</div>
 
-			<g-link
-				v-if="next"
-				:to="next.path"
-				:class="[
-					'px-4 py-2',
-					'flex items-center',
-					'rounded-lg',
-					'font-bold',
-					'text-on-button bg-button',
-					'hover:text-on-button-hover',
-					'transition-colors duration-200',
-				]"
-			>
-				{{ next.title }}
-				<icon icon="rightArrow" class="ml-2 text-2xl" />
-			</g-link>
+			<div>
+				<g-link
+					v-if="next"
+					style="place-self: end"
+					:to="next.path"
+					:class="[
+						'w-full px-4 py-2',
+						'flex items-center justify-center sm:justify-start',
+						'rounded-lg',
+						'font-bold',
+						'text-on-button bg-button',
+						'hover:text-on-button-hover',
+						'transition-colors duration-200',
+					]"
+				>
+					{{ next.title }}
+					<icon icon="rightArrow" class="ml-2 text-2xl" />
+				</g-link>
+			</div>
 		</div>
 	</div>
 </template>
