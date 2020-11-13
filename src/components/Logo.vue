9ff4344b38d2441126c69ab2083edee7693fe5f8
Enzo Innocenzi <enzo@innocenzi.dev>
2020-11-13 19:12:06 +0100

    refactor: update logo

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,35 +1,29 @@
 <template>
-	<svg
-		:width="getWidth"
-		:height="getHeight"
-		viewBox="0 0 283 566"
-		fill="currentColor"
-		xmlns="http://www.w3.org/2000/svg"
-	>
-		<path
-			d="M128.603 76.9091L129.33 154.364L51.2234 154.364L51.9499 0.181822L179.645 0C222.15 0 256.843 34.7273 257.207 77.6364L128.603 76.9091ZM51.0417 411.636L128.785 410.545L128.603 283.091L51.9499 283.091L51.0417 411.636ZM154.942 128.727L283 128.545L282.637 179.818L257.207 179.818L256.117 566H154.033L154.215 257.455L0 256.545V179.818L154.942 180L154.942 128.727Z"
-			fill="#7F9CF5"
-		/>
+	<svg :width="getWidth" :height="getHeight" viewBox="0 0 640 640" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path d="M88 319.843L229.421 178.421L370.843 319.843L229.421 461.264L88 319.843Z" fill="#7F9CF5" />
+		<path d="M229.421 178.421L370.843 37L512.264 178.421L370.843 319.843L229.421 178.421Z" fill="#667EEA" />
+		<path d="M229.421 461.264L370.843 319.843L512.264 461.264L370.843 602.685L229.421 461.264Z" fill="#A3BFFA" />
 	</svg>
 </template>
 
-<script>
-export default {
-	props: {
-		width: {
-			type: Number,
-			default: 40,
-		},
-	},
+	<script>
+		export default {
+			props: {
+				width: {
+					type: Number,
+					default: 40,
+				},
+			},
 
-	computed: {
-		getWidth() {
-			return this.width;
-		},
+			computed: {
+				getWidth() {
+					return this.width;
+				},
 
-		getHeight() {
-			return Math.ceil((this.getWidth / 48) * 48);
-		},
-	},
-};
-</script>
+				getHeight() {
+					return Math.ceil((this.getWidth / 48) * 48);
+				},
+			},
+		};
+	</script>
+</template>
